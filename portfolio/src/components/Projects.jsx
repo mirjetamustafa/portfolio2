264f861ce0cf9cb4a26f87666edafae35885f152
Mirjeta Mustafa <mustafamirjeta11@gmail.com>
2025-08-04 15:44:05 +0200

    Updated projects

--- portfolio/src/components/Projects.jsx
+++ portfolio/src/components/Projects.jsx
@@ -10,20 +10,46 @@ function Projects() {
             <span className="bg-blue-600 w-5 h-5 mt-3 mx-2"></span>
             <h1 className="text-4xl font-bold"> Projects</h1>
           </div>
-          <div className="flex mt-9 -ml-9">
-            <Link to={'https://gjirafamall-clone.netlify.app/'}>
-              <a className="bg-blue-600 border border-blue-600 text-white font-bold text-sm rounded-full px-4 py-1 hover:text-black hover:bg-[#e8dccf]">
-                GjirafaMall Clone
-              </a>
-            </Link>
-            <Link
-              to={'https://papaya-lamington-e122df.netlify.app/'}
-              className="mx-5"
+          <div className="flex flex-col  ml-5">
+            <a
+              href="https://gjirafamall-clone.netlify.app/"
+              target="_blank"
+              className="bg-blue-600 border border-blue-600 mt-9  text-white font-bold text-sm rounded-full px-4 py-1 hover:text-black hover:bg-[#e8dccf]"
             >
-              <a className="bg-blue-600 border border-blue-600 text-white font-bold text-sm rounded-full px-4 py-1 hover:text-black hover:bg-[#e8dccf]">
-                FundedNext Clone
-              </a>
-            </Link>
+              GjirafaMall Clone
+            </a>
+
+            <a
+              href="https://papaya-lamington-e122df.netlify.app/"
+              target="_blank"
+              className="bg-blue-600 border border-blue-600 mt-5  text-white font-bold text-sm rounded-full px-4 py-1 hover:text-black hover:bg-[#e8dccf]"
+            >
+              FundedNext Clone
+            </a>
+
+            <a
+              href="https://papaya-lamington-e122df.netlify.app/"
+              target="_blank"
+              className="bg-blue-600 border border-blue-600 mt-5 text-white font-bold text-sm rounded-full px-4 py-1 hover:text-black hover:bg-[#e8dccf]"
+            >
+              Admin Dashboard
+            </a>
+
+            <a
+              href="https://papaya-lamington-e122df.netlify.app/"
+              target="_blank"
+              className="bg-blue-600 border border-blue-600 mt-5 text-white font-bold text-sm rounded-full px-4 py-1 hover:text-black hover:bg-[#e8dccf]"
+            >
+              Movie Flix
+            </a>
+
+            <a
+              href="https://papaya-lamington-e122df.netlify.app/"
+              target="_blank"
+              className="bg-blue-600 border border-blue-600 mt-5 text-white font-bold text-sm rounded-full px-4 py-1 hover:text-black hover:bg-[#e8dccf]"
+            >
+              Luxury Cars
+            </a>
           </div>
         </div>
       </div>
